039c764f8837f0bac0aa64c91ac7bb7c89aa33b4
Rafael Rodrigues <rafael.rodrigues@infor.com>
2021-07-30 15:18:50 -0400

    reset code

---
+++ README.md
@@ -0,0 +1,52 @@
+# Convert To AWS SSM
+
+Convert Powershell and/or Shell scripts to AWS SSM documents.
+
+## Features
+
+This plugin is capable of generating both JSON and YAML AWS SSM Documents from PowerShell and/or Shell scripts.
+
+## Usage
+
+1. Open a PowerShell or Shell script of your choice
+2. Open the Command Palette (F1 or CMD/CTRL+Shift+P or "View" > "Command Palette...")
+3. Search for "ConvertTo-SSM", and two options will appear
+    - "ConvertTo-SSM: Create JSON document"
+    - "ConvertTo-SSM: Create YAML document"
+4. Click desired conversion option
+
+## Configurations
+
+- EnableAutoCopyToClipboard
+    When enabled, the SSM document output is automatically copied to the clipboard
+
+- EnableCopyRunCommandOnly
+    When enabled alongside EnableAutoCopyToClipboard, it copies the 'RunCommand' section of the SSM command only
+
+## Instructions
+
+### PowerShell To JSON SSM Documnet
+
+![PowerShell to Json](./gifs/ConvertPowerShellToJson.gif)
+
+### Shell script To JSON SSM Documnet
+
+![Shell to Json](./gifs/ConvertShellToJson.gif)
+
+### PowerShell to YAML SSM document
+
+![PowerShell to Json](./gifs/ConvertPowerShellToYaml.gif)
+
+### Shell script to YAML SSM document
+
+![Shell to Json](./gifs/ConvertShellToYaml.gif)
+
+## Known Issues
+
+N/A
+
+## Release Notes
+
+### 0.0.4
+
+Pre-release
---
+++ src/ConvertToSSM/Util.ts
@@ -0,0 +1,118 @@
+import * as vscode from 'vscode';
+
+export function getDocumentIndent(): number {
+
+  const workspaceConfig = vscode.workspace.getConfiguration('editor');
+  if (workspaceConfig && workspaceConfig.get('insertSpaces')) { //if insert spaces is true
+    const tabSize = workspaceConfig.get('tabSize'); //get tabsize
+    if (tabSize && typeof tabSize === 'number') { //ensure tabsize is indeed a number
+      return tabSize;
+    }
+  }
+  return 4;
+}
+
+
+function getExtensionConfig(): any {
+  let extensionConfig = vscode.workspace.getConfiguration('convertto-ssm');
+  if (extensionConfig) {
+    return extensionConfig;
+  }
+  return {};
+}
+
+export function isAutoCopyEnabled(): boolean {
+  let extensionConfig = getExtensionConfig();
+  if (extensionConfig && extensionConfig.get('EnableAutoCopyToClipboard')) { //if insert spaces is true
+    let autoCopyEnabled = extensionConfig.get('EnableAutoCopyToClipboard') ;
+    if (autoCopyEnabled && typeof autoCopyEnabled === 'boolean'){
+      return autoCopyEnabled;
+    }
+  }
+  return false;
+}
+
+export function copyRunCommandOnly(): boolean {
+  let extensionConfig = getExtensionConfig();
+  if (extensionConfig && extensionConfig.get('EnableCopyRunCommandOnly')) { //if insert spaces is true
+    let copyRunCmd = extensionConfig.get('EnableCopyRunCommandOnly') ;
+    if (copyRunCmd && typeof copyRunCmd === 'boolean'){
+      return copyRunCmd;
+    }
+  }
+  return false;
+}
+
+
+export function openNewFile(content: string, language: string = 'json') {
+  vscode.workspace.openTextDocument({
+    language: language
+  })
+    .then(doc => vscode.window.showTextDocument(doc))
+    .then(editor => {
+      editor.edit((editBuilder: vscode.TextEditorEdit) => {
+        editBuilder.insert(new vscode.Position(0, 0), content);
+      });
+    });
+}
+
+export function copyToClipboard(content: string){
+  vscode.env.clipboard.writeText(content);
+}
+
+export function validate(languageId: string): boolean {
+  if (!(['powershell', 'shellscript'].includes(languageId))) {
+    vscode.window.showErrorMessage('Only PowerShell and Shell scripts supported :(');
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+export interface DocumentStructure {
+  schemaVersion: string;
+  description: string;
+  parameters: Object;
+  mainSteps: [{
+    precondition: {
+      StringEquals: string[];
+    };
+    action: string;
+    name: string;
+    inputs: {
+      workingDirectory: string;
+      timeoutSeconds: string;
+      runCommand?: string[];
+    }
+  }];
+}
+
+export const defaultDoc: DocumentStructure = {
+  "schemaVersion": "2.2",
+  "description": "Document Description",
+  "parameters": {
+    "sampleParameter": {
+      "type": "String",
+      "description": "SampleParameter description",
+      "default": "defaultParamValue"
+    }
+  },
+  "mainSteps": [
+    {
+      "precondition": {
+        "StringEquals": [
+          "platformType",
+          "Windows"
+        ]
+      },
+      "action": "aws:runPowerShellScript",
+      "name": "StepName",
+      "inputs": {
+        "workingDirectory": "",
+        "timeoutSeconds": "400",
+        "runCommand": undefined
+      }
+    }
+  ]
+};
